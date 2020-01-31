766bf2150c5f8559682817beedcb3d7e296e442c
Tolga KALAYCIOĞLU <47540955+tolgak99@users.noreply.github.com>
2020-02-01 02:02:36 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//  Created by Tolga KALAYCIOĞLU.
+//  Copyright © 2020. All rights reserved.
+
+#include <iostream>
+#include <string>
+#include "Menu.h"
+
+
+using namespace std;
+
+
+int main() {
+
+	Menu m;
+
+	m.show_menu();
+
+
+
+	return(0);
+}
